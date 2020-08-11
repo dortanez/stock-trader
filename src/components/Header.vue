c883355781cb562efdfc851769384453f8837ab3
Danisa Ortanez <dortanez@yahoo.com>
2020-08-10 19:42:10 -0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="navbar">
+  <div class="navbar-inner">
+    <router-link to='/' class='navbar-brand'>Stock Trader</router-link>
+    <ul class="nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+    </ul>
+  </div>
+</div>
+</template>
